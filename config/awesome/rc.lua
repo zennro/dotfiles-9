7c813554f89b2cc4ee62d49371b365f45b04ae60
Colin Bell <col@baibell.org>
2011-04-28 18:09:08 +1000

    awesome wm

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -58,7 +58,7 @@ layouts =
 -- Define a tag table which hold all screen tags.
 tags = {
    names  = {"1", "2", "3", "4", "5", "6", "7", "8", "9"},
-   layout = {layouts[1], layouts[2], layouts[3], layouts[4], layouts[5], layouts[6], layouts[7], layouts[8], layouts[9] }
+   layout = {layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1] }
 }
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
