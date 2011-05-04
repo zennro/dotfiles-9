9b1ac1e56875c3a06d07751acdc7cdfdd08c8e9c
Colin Bell <col@baibell.org>
2011-05-04 18:40:58 +1000

    config

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -6,8 +6,10 @@
 require("awful")
 require("awful.autofocus")
 require("awful.rules")
+
 -- Theme handling library
 require("beautiful")
+
 -- Notification library
 require("naughty")
 
@@ -68,12 +70,12 @@ end
 
 -- {{{ Menu
 -- Create a laucher widget and a main menu
-myawesomemenu = {
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awful.util.getdir("config") .. "/rc.lua" },
-   { "restart", awesome.restart },
-   { "quit", awesome.quit }
-}
+-- myawesomemenu = {
+--    { "manual", terminal .. " -e man awesome" },
+--    { "edit config", editor_cmd .. " " .. awful.util.getdir("config") .. "/rc.lua" },
+--    { "restart", awesome.restart },
+--    { "quit", awesome.quit }
+-- }
 
 -- mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
 --                                     { "open terminal", terminal }
