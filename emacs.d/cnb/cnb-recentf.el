0f1856b39f4135102ace30b29aa6fb6603bc9057
Colin Bell <col@baibell.org>
2012-12-21 10:38:27 +1100

    Emacs - recentf mode config

--- emacs.d/cnb/cnb-recentf.el
+++ emacs.d/cnb/cnb-recentf.el
@@ -1,15 +1,20 @@
 (require 'recentf)
 
+(setq recentf-save-file (file-truename (expand-file-name "recentf" user-emacs-directory)))
 (setq recentf-max-saved-items 200)
 (setq recentf-exclude '("~$"))
 
-(add-to-list 'recentf-exclude "~/.emacs.d/elpa/.*")
-(add-to-list 'recentf-exclude "~/.ido.last")
-(add-to-list 'recentf-exclude "~/.emacs.d/recentf")
-(add-to-list 'recentf-exclude "~/.emacs.d/places")
-(add-to-list 'recentf-exclude "~/.emacs.d/elpa/.*")
+(add-to-list 'recentf-exclude recentf-save-file)
 
-(setq recentf-save-file "~/.emacs.d/recentf")
+;; Because .emacs.d is a symlink to dotfiles/.emacs.d a file can have two names so
+;; we need to ignore each file twice. The function (file-truename) will expand symlinks.
+(setq exclude-files '("places" ".ido.last" "elpa/**/.*" "emacs.bmk" "url/cookies"))
+(while exclude-files
+  (setq f (car exclude-files))
+  (setq f (expand-file-name f user-emacs-directory))
+  (add-to-list 'recentf-exclude f)
+  (add-to-list 'recentf-exclude (file-truename f))
+  (setq exclude-files (cdr exclude-files)))
 
 (recentf-mode 1)
 
