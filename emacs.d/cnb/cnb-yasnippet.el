7be8fc141d7250047abf0f9fd678c47af98f21ca
Colin Bell <col@baibell.org>
2012-12-21 10:38:09 +1100

    Emacs - yasnippets

--- emacs.d/cnb/cnb-yasnippet.el
+++ emacs.d/cnb/cnb-yasnippet.el
@@ -1,6 +1,5 @@
 (when (require 'yasnippet nil t)
-  ;; causes "Don't know how to make a localized variable an alias" error.
-  ;;(yas-global-mode 1)
+  (yas-global-mode 1)
 )
 
 (provide 'cnb-yasnippet)
