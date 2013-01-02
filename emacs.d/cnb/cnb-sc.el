1f78c18d90e1cef16c5e844f7b703a5274127376
Colin Bell <col@baibell.org>
2013-01-03 09:05:16 +1100

    Magit

--- emacs.d/cnb/cnb-sc.el
+++ emacs.d/cnb/cnb-sc.el
@@ -14,6 +14,23 @@
 
 (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
 
 (add-hook 'magit-log-edit-mode-hook 'flyspell-mode)
 (add-hook 'git-commit-mode-hook 'flyspell-mode)
