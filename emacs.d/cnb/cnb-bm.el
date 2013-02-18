da458b4fd2e2aa2947d52d232a69145c023e3ac3
Colin Bell <col@baibell.org>
2013-02-19 09:03:26 +1100

    Emacs - bookmarks

--- emacs.d/cnb/cnb-bm.el
+++ emacs.d/cnb/cnb-bm.el
@@ -2,32 +2,31 @@
 (setq bookmark-default-file (expand-file-name "emacs.bmk" user-emacs-directory))
 
 (setq bm-restore-repository-on-load t)
-(when (require 'bm nil t)
-  (setq bm-repository-file (expand-file-name "bm-repository" user-emacs-directory))
-  (setq bm-repository-size 1024)
+(setq bm-repository-file (expand-file-name "bm-repository" user-emacs-directory))
+(setq bm-repository-size 1024)
 
+(when (require 'bm nil t)
   (when (fboundp 'bm-toggle)
     (global-set-key (kbd "<C-f2>") 'bm-toggle)
     (global-set-key (kbd "<f2>")   'bm-next)
     (global-set-key (kbd "<S-f2>") 'bm-previous))
 
-  ;; (setq-default bm-buffer-persistence t)
+  (setq-default bm-buffer-persistence t)
 
   ;; Load the repository from file on start up.
-  ;; (add-hook' after-init-hook 'bm-repository-load)
+  (add-hook' after-init-hook 'bm-repository-load)
 
   ;; Restore bookmarks on file find.
-  ;; (add-hook 'find-file-hooks 'bm-buffer-restore)
+  (add-hook 'find-file-hooks 'bm-buffer-restore)
 
   ;; Save bookmark data when killing a buffer
-  ;;(add-hook 'kill-buffer-hook 'bm-buffer-save)
+  (add-hook 'kill-buffer-hook 'bm-buffer-save)
 
   ;; Save the repository to file on exit.
-  ;; (add-hook 'kill-emacs-hook '(lambda nil
-  ;;                               (bm-buffer-save-all)
-  ;;                               (bm-repository-save)))
+  (add-hook 'kill-emacs-hook '(lambda nil
+                                (bm-buffer-save-all)
+                                (bm-repository-save)))
 
   (setq bm-highlight-style 'bm-highlight-only-fringe))
-
 
 (provide 'cnb-bm)
