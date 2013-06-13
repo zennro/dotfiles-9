533feee56910da986f533d34816e81d4cba283b1
Colin Bell <col@baibell.org>
2013-06-13 19:47:58 +1000

    Fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,6 +9,8 @@ set -U __fish_git_prompt_char_stashstate '↩'
 set -U __fish_git_prompt_char_upstream_ahead '↑'
 set -U __fish_git_prompt_char_upstream_behind '↓'
 
+set -x TERM xterm-256color
+
 # make less more friendly for non-text input files, see lesspipe(1)
 if test -x "/usr/bin/lesspipe"
   set -x LESSOPEN "| lesspipe %s"
@@ -18,15 +20,13 @@ set -x LESS ' -R -X'
 set -x PAGER 'less -i'
 
 # Enable coloured man pages
-set LESS_TERMCAP_mb '\e[01;31m'
-set LESS_TERMCAP_md '\e[01;31m'
-set LESS_TERMCAP_me '\e[0m'
-set LESS_TERMCAP_se '\e[0m'
-set LESS_TERMCAP_so '\e[01;44;33m'
-set LESS_TERMCAP_ue '\e[0m'
-set LESS_TERMCAP_us '\e[01;32m'
-
-set -x TERM xterm-256color
+# set LESS_TERMCAP_mb '\e[01;31m'
+# set LESS_TERMCAP_md '\e[01;31m'
+# set LESS_TERMCAP_me '\e[0m'
+# set LESS_TERMCAP_se '\e[0m'
+# set LESS_TERMCAP_so '\e[01;44;33m'
+# set LESS_TERMCAP_ue '\e[0m'
+# set LESS_TERMCAP_us '\e[01;32m'
 
 set -x MANWIDTH 96
 
