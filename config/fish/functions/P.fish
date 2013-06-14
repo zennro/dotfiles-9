d6ba95688d4701fe799df98c244ecef01d781e35
Colin Bell <col@baibell.org>
2013-06-14 16:49:33 +1000

    Spelling

--- config/fish/functions/P.fish
+++ config/fish/functions/P.fish
@@ -1,5 +1,4 @@
 function P --description 'Display an easy to read $PATH'
-
-  # This won't work if ant directories have spaces in the name.'
+  # This won't work if any directories have spaces in the name.'
   echo $PATH | tr -s ' ' '\n'
 end
