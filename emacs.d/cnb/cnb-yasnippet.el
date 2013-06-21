2374466d912a1763a2e68aa0f4cc3b60a1383d1b
Colin Bell <col@baibell.org>
2013-06-21 11:21:40 +1000

    Emacs - yasnippets

--- emacs.d/cnb/cnb-yasnippet.el
+++ emacs.d/cnb/cnb-yasnippet.el
@@ -1,10 +1,10 @@
 (when (require 'yasnippet nil t)
   (yas-global-mode 1)
-  (require 'helm-c-yasnippet nil t)
+  ;;(require 'helm-c-yasnippet nil t)
   (setq yas-prompt-functions '(yas-ido-prompt
                                yas-completing-prompt)))
 
 (when (fboundp 'yas-insert-snippet)
-  (global-set-key (kbd "C-c y") 'helm-c-yas-complete))
+  (global-set-key (kbd "C-c y") 'yas-insert-snippet))
 
 (provide 'cnb-yasnippet)
