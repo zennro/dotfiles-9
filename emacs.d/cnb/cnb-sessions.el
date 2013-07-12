f1cc8aa6b779edb86f4a6bbd0ec126d1da96c563
Colin Bell <col@baibell.org>
2013-07-12 10:26:09 +1000

    Emacs - sessions

--- emacs.d/cnb/cnb-sessions.el
+++ emacs.d/cnb/cnb-sessions.el
@@ -4,6 +4,9 @@
 (setq save-place-file (expand-file-name "places" user-emacs-directory))
 
 ;; Remember open buffers
+(require 'desktop)
+(setq desktop-load-locked-desktop nil) ;; FIXME: This isn't working - always set to ask
+(setq desktop-restore-eager 5) ;; Open first X immed then rest in background
 (desktop-save-mode 1)
 
 ;; Remember mini-buffer history
