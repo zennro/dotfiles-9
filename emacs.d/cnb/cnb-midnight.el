5b16b5c44d21bcb3dc10ccdbae7742fa6ec82049
Colin Bell <col@baibell.org>
2014-02-28 08:56:11 +1100

    Emacs

--- emacs.d/cnb/cnb-midnight.el
+++ emacs.d/cnb/cnb-midnight.el
@@ -4,7 +4,7 @@
 ;; Purge unused buffers
 ;;==============
 (require 'midnight)
-(add-to-list 'clean-buffer-list-kill-never-buffer-names "*info*")
-(add-to-list 'clean-buffer-list-kill-never-buffer-names "*scratch*")
+;; (add-to-list 'clean-buffer-list-kill-never-buffer-names "*info*")
+;; (add-to-list 'clean-buffer-list-kill-never-buffer-names "*scratch*")
 
 (provide 'cnb-midnight)
