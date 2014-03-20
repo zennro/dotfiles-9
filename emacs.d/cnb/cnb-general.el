922e72494aa7ec093dbfdd9e1232710153f2ad6e
Colin Bell <col@baibell.org>
2014-03-21 09:33:07 +1100

    Emacs

--- emacs.d/cnb/cnb-general.el
+++ emacs.d/cnb/cnb-general.el
@@ -180,9 +180,10 @@
 ;; hippie-expand
 ;;==============
 (setq hippie-expand-try-functions-list
-      '(try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs
-                                         try-expand-dabbrev try-expand-dabbrev-all-buffers
-                                         try-expand-dabbrev-from-kill try-complete-lisp-symbol))
+      '(try-complete-file-name-partially
+        try-complete-file-name try-expand-all-abbrevs
+        try-expand-dabbrev try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill try-complete-lisp-symbol))
 
 ;;==============
 ;; multiple cursors
