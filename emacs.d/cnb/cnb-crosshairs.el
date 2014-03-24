4d7a8a97c65ca9d97208cc9e2c694980130e2eb7
Colin Bell <col@baibell.org>
2014-03-25 10:55:30 +1100

    Emacs

--- emacs.d/cnb/cnb-crosshairs.el
+++ emacs.d/cnb/cnb-crosshairs.el
@@ -1,23 +1,19 @@
 (message (concat "[CNB] - Loading [" load-file-name "]"))
 
 (when (require 'crosshairs nil t)
+  ;; Too irritating
   ;;(toggle-crosshairs-when-idle 1)
 
-  ;;(global-hl-line-mode -1)
-
-  ;; When these are turned on I get the following error:
-  ;; Without this we get a constant - Error in post-command-hook (global-hl-line-highlight): (wrong-type-argument overlayp nil)
-
   ;; FIXME: Use ad-add-advice to apply to a list of functions?
-  ;; (defadvice switch-to-buffer (after switch-to-buffer-flash-crosshairs activate)
-  ;;   "Call flash-crosshairs after switch-to-buffer"
-  ;;   (flash-crosshairs))
-  ;; (defadvice icicle-other-window-or-frame (after icicle-other-window-or-frame-flash-crosshairs activate)
-  ;;   "Call flash-crosshairs after icicle-other-window-or-frame"
-  ;;   (flash-crosshairs))
-  ;; (defadvice windmove-do-window-select (after windmove-do-flash-crosshairs activate)
-  ;;   "Call flash-crosshairs after windmove-do-window-select"
-  ;;   (flash-crosshairs))
+  (defadvice switch-to-buffer (after switch-to-buffer-flash-crosshairs activate)
+    "Call flash-crosshairs after switch-to-buffer"
+    (flash-crosshairs))
+  (defadvice icicle-other-window-or-frame (after icicle-other-window-or-frame-flash-crosshairs activate)
+    "Call flash-crosshairs after icicle-other-window-or-frame"
+    (flash-crosshairs))
+  (defadvice windmove-do-window-select (after windmove-do-flash-crosshairs activate)
+    "Call flash-crosshairs after windmove-do-window-select"
+    (flash-crosshairs))
 
   (global-set-key (kbd "<f11>") 'flash-crosshairs))
 
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -73,7 +73,11 @@
 (require 'cnb-print)           ;; Printing
 (require 'cnb-scrolling)       ;; Scrolling
 (require 'cnb-ace)             ;; Ace jump
-(require 'cnb-crosshairs)      ;; Screen crosshairs
+
+;; Causes the following in Emacs 24.4.50.2
+;; Error in post-command-hook (global-hl-line-highlight): (wrong-type-argument overlayp nil)
+;;(require 'cnb-crosshairs)      ;; Screen crosshairs
+
 (require 'cnb-edit-server)     ;; Edit within Chrome
 (require 'cnb-smartparens)     ;; Smart parens
 ;;(require 'cnb-mu4e)            ;; Email
