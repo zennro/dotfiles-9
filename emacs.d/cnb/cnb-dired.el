b646ae37c60efaa41b04696444b927f5c37fecc0
Colin Bell <col@baibell.org>
2014-03-25 17:19:35 +1100

    Emacs - dired

--- emacs.d/cnb/cnb-dired.el
+++ emacs.d/cnb/cnb-dired.el
@@ -8,22 +8,34 @@
 (setq dired-recursive-copies 'always)
 (setq dired-recursive-deletes 'always)
 
-;; From http://whattheemacsd.com/
-(defun dired-back-to-top ()
+(defun cnb-dired-back-to-top ()
+  "Move to the first file name in the dired buffer"
   (interactive)
-  (beginning-of-buffer)
-  (dired-next-line 4))
 
+  ; If dired-hide-details-mode enabled then there are 3 lines of
+  ; headings, else 4 lines.
+  (let (line-nbr)
+    (if (and (boundp 'dired-hide-details-mode) dired-hide-details-mode)
+        (setq line-nbr 3)
+      (setq line-nbr 4))
+    (beginning-of-buffer)
+    (dired-next-line line-nbr)))
+
+;; Change M-< instead of moving to the dired headings move to the first
+;; file name.
 (define-key dired-mode-map
-  (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
+  (vector 'remap 'beginning-of-buffer) 'cnb-dired-back-to-top)
 
-(defun dired-jump-to-bottom ()
+(defun cnb-dired-jump-to-bottom ()
+  "Jump to last file in dired buffer"
   (interactive)
   (end-of-buffer)
   (dired-next-line -1))
 
+;; Change M-> instead of moving to empty line at bottom  move to the last
+;; file name.
 (define-key dired-mode-map
-  (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
+  (vector 'remap 'end-of-buffer) 'cnb-dired-jump-to-bottom)
 
 ;; Allow <ALT>o to omit hidden files
 (require 'dired-x)
@@ -34,6 +46,6 @@
 (setq wdired-allow-to-change-permissions 't)
 (setq wdired-confirm-overwrite 't)
 
-;;(global-set-key (kbd "C-c C-d") 'dired-jump)
+(global-set-key (kbd "C-c C-d") 'dired-jump)
 
 (provide 'cnb-dired)
