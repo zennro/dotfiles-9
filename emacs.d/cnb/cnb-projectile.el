d52d9cc14ea1a6e01d811a2541246dd77c50942b
Colin Bell <col@baibell.org>
2014-04-23 10:39:35 +1000

    Emacs - Use helm for projectile

--- emacs.d/cnb/cnb-projectile.el
+++ emacs.d/cnb/cnb-projectile.el
@@ -6,7 +6,7 @@
   ;;(require 'grizzl)
   (projectile-global-mode)
   ;;(setq projectile-enable-caching t)
-  (setq projectile-completion-system 'default)) ;; Use icicles for completion
+  (setq projectile-completion-system 'helm))
   ;;(setq projectile-completion-system 'grizzl)
   ;; Press Command-p for fuzzy find in project
   ;; Press Command-b for fuzzy switch buffer
