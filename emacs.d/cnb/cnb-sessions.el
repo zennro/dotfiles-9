af78b54a8d777a4a4e5c93563e0c58650c741a5d
Colin Bell <col@baibell.org>
2014-04-23 10:38:28 +1000

    Emacs - don't restore buffers on startup

--- emacs.d/cnb/cnb-sessions.el
+++ emacs.d/cnb/cnb-sessions.el
@@ -10,19 +10,18 @@
 
 ;;;; desktop
 
-;; Remember open buffers
-(require 'desktop)
-(desktop-save-mode 1)
-(setq desktop-load-locked-desktop nil)
-(setq desktop-restore-eager 10) ;; Open first X immed then rest in background
-;;(setq desktop-restore-eager t)
-(setq history-length 250)
-
-(add-to-list 'desktop-modes-not-to-save 'dired-mode)
+;;  Remember open buffers
+;; (require 'desktop)
+;; (desktop-save-mode 1)
+;; (setq desktop-load-locked-desktop nil)
+;; (setq desktop-restore-eager 5) ;; Open first X immed then rest in background
+;; ;;(setq desktop-restore-eager t)
+;; (add-to-list 'desktop-modes-not-to-save 'dired-mode)
 
 ;;;; savehist
 
 ;; Remember mini-buffer history
+(setq history-length 250)
 (setq savehist-additional-variables
       '(search-ring regexp-search-ring kill-ring compile-history))
 (savehist-mode t)
