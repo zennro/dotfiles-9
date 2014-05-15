9ce0c6845f7f167b052a38d7e5c05ff73d27c022
Colin Bell <col@baibell.org>
2014-05-15 11:28:24 +1000

    xmenud

--- bin/xmenud.py
+++ bin/xmenud.py
@@ -167,8 +167,8 @@ def main():
             use_icons = False
 
     try:
-        desktopmenu = xdg.Menu.parse("/etc/xdg/menus/xfce-applications.menu")
-        #desktopmenu = xdg.Menu.parse("/etc/xdg/menus/kde4-applications.menu")
+        #desktopmenu = xdg.Menu.parse("/etc/xdg/menus/xfce-applications.menu")
+        desktopmenu = xdg.Menu.parse("/etc/xdg/menus/kde4-applications.menu")
         #desktopmenu = xdg.Menu.parse("/etc/xdg/menus/kde-information.menu")
         #desktopmenu = xdg.Menu.parse("/etc/xdg/menus/gnome-applications.menu")
     except xdg.Exceptions.ParsingError:
