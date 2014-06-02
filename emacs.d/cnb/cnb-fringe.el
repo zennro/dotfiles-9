6433a0085a57ba5a6f56fc114222f92bbe7d4341
Colin Bell <col@baibell.org>
2014-06-02 11:09:14 +1000

    EMACS - Don't show boundaries in fringe

--- emacs.d/cnb/cnb-fringe.el
+++ emacs.d/cnb/cnb-fringe.el
@@ -5,7 +5,7 @@
 (when (fboundp 'global-git-gutter-mode)
   (global-git-gutter-mode))
 
-(setq-default indicate-buffer-boundaries 'left)
+(setq-default indicate-buffer-boundaries nil)
 (setq-default indicate-empty-lines t)
 
 (provide 'cnb-fringe)
