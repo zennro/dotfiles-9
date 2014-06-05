42c6cf6649a5994f3fc42c0cf2b1393d35fe1c6b
Colin Bell <col@baibell.org>
2014-06-05 20:15:21 +1000

    EMACS - sessions

--- emacs.d/cnb/cnb-sessions.el
+++ emacs.d/cnb/cnb-sessions.el
@@ -11,12 +11,12 @@
 ;;;; desktop
 
 ;; Remember open buffers
-(require 'desktop)
-(desktop-save-mode 1)
-(setq desktop-load-locked-desktop nil)
-(setq desktop-restore-eager 5) ;; Open first X immed then rest in background
-;;(setq desktop-restore-eager t)
-(add-to-list 'desktop-modes-not-to-save 'dired-mode)
+;; (require 'desktop)
+;; (desktop-save-mode 1)
+;; (setq desktop-load-locked-desktop nil)
+;; (setq desktop-restore-eager 5) ;; Open first X immed then rest in background
+;; ;;(setq desktop-restore-eager t)
+;; (add-to-list 'desktop-modes-not-to-save 'dired-mode)
 
 ;;;; savehist
 
