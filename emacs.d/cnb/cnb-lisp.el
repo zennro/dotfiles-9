6b421b11023d1420127227ef3967ad2365d0ca8e
Colin Bell <col@baibell.org>
2014-06-17 10:22:25 +1000

    EMACS - cider

--- emacs.d/cnb/cnb-lisp.el
+++ emacs.d/cnb/cnb-lisp.el
@@ -33,8 +33,9 @@
      (require 'cider-eldoc)
      (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
      (add-hook 'cider-repl-mode-hook 'subword-mode)
-
+     (setq nrepl-hide-special-buffers t)
      (setq cider-repl-history-file "~/.emacs.d/cider-repl-history")
+     (setq cider-show-error-buffer t)
      (setq cider-repl-history-size 1000)))
 
 
