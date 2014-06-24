0bb311bcd308e85b56d231b67bf3de71bfc42d2a
Colin Bell <col@baibell.org>
2014-06-24 11:06:50 +1000

    EMACS - aliases

--- emacs.d/cnb/cnb-aliases.el
+++ emacs.d/cnb/cnb-aliases.el
@@ -3,5 +3,6 @@
 (defalias 'dtw 'delete-trailing-whitespace)
 (defalias 'wsc 'whitespace-cleanup)
 (defalias 'vlm 'visual-line-mode)
+(defalias 'cveb 'cider-visit-error-buffer)
 
 (provide 'cnb-aliases)
