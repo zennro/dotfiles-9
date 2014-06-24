13f1e69b185b8f7f9f64464e2905492f429de412
Colin Bell <col@baibell.org>
2014-06-24 11:08:27 +1000

    EMACS - Clojure

--- emacs.d/cnb/cnb-lisp.el
+++ emacs.d/cnb/cnb-lisp.el
@@ -22,22 +22,32 @@
 
 
 ;;;; CLOJURE
-(require 'clojure-mode-extra-font-locking nil t)
+(when (require 'clojure-mode nil t)
+  (add-hook 'clojure-mode-hook 'cider-mode)
+  (require 'clojure-mode-extra-font-locking nil t))
 
 
 ;;;; CIDER
-(require 'cider nil t)
-(eval-after-load 'cider-mode
-  '(progn
-     (setq nrepl-log-messages t)
-     (require 'cider-eldoc)
-     (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-     (add-hook 'cider-repl-mode-hook 'subword-mode)
-     (setq nrepl-hide-special-buffers t)
-     (setq cider-repl-history-file "~/.emacs.d/cider-repl-history")
-     (setq cider-show-error-buffer t)
-     (setq cider-repl-history-size 1000)))
+(when (require 'cider nil t)
+  (setq nrepl-log-messages t)
+  (require 'cider-eldoc)
+  (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+  (add-hook 'cider-repl-mode-hook 'subword-mode)
+  ;;(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+  (setq cider-repl-use-clojure-font-lock t)
+  (setq nrepl-hide-special-buffers t)
+  (setq cider-show-error-buffer nil)
+  (setq cider-auto-select-error-buffer nil)
+  (setq cider-repl-history-file "~/.emacs.d/cider-repl-history")
+  (setq cider-repl-history-size 1000)
 
+  (defun cider-ns-refresh ()
+    (interactive)
+    (cider-interactive-eval
+     "(require 'clojure.tools.namespace.repl)
+         (clojure.tools.namespace.repl/refresh)"))
+
+  (define-key clojure-mode-map (kbd "M-r") 'cider-ns-refresh))
 
 ;;;; SLIME
 
