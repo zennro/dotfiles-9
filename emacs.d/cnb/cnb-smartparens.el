4c932ab170d64f8c81ca3ed2acebe01a259b0476
Colin Bell <col@baibell.org>
2014-07-06 19:31:55 +1000

    EMACS - smartparens

--- emacs.d/cnb/cnb-smartparens.el
+++ emacs.d/cnb/cnb-smartparens.el
@@ -5,12 +5,21 @@
   (require 'smartparens-html)
   (require 'smartparens-latex)
 
-  (smartparens-global-mode)
+  (smartparens-global-mode t)
 
   (show-smartparens-global-mode t)
 
   (sp-with-modes '(rhtml-mode)
     (sp-local-pair "<" ">")
-    (sp-local-pair "<%" "%>")))
+    (sp-local-pair "<%" "%>"))
+
+  (define-key sp-keymap (kbd "C-M-f") 'sp-forward-sexp)
+  (define-key sp-keymap (kbd "C-M-b") 'sp-backward-sexp)
+
+  (define-key sp-keymap (kbd "C-M-d") 'sp-down-sexp)
+  (define-key sp-keymap (kbd "C-M-a") 'sp-backward-down-sexp)
+  (define-key sp-keymap (kbd "C-S-a") 'sp-beginning-of-sexp)
+  (define-key sp-keymap (kbd "C-S-d") 'sp-end-of-sexp)
+  )
 
 (provide 'cnb-smartparens)
