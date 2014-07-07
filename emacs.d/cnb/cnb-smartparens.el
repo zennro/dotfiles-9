64504d8cadff23b6e347cfaae04467a076a8febf
Colin Bell <col@baibell.org>
2014-07-07 21:10:59 +1000

    EMACS - smart-parens

--- emacs.d/cnb/cnb-smartparens.el
+++ emacs.d/cnb/cnb-smartparens.el
@@ -13,6 +13,8 @@
     (sp-local-pair "<" ">")
     (sp-local-pair "<%" "%>"))
 
+  (setq sp-autoescape-string-quote nil) ; Irritating.
+
   (define-key sp-keymap (kbd "C-M-f") 'sp-forward-sexp)
   (define-key sp-keymap (kbd "C-M-b") 'sp-backward-sexp)
 
@@ -20,6 +22,16 @@
   (define-key sp-keymap (kbd "C-M-a") 'sp-backward-down-sexp)
   (define-key sp-keymap (kbd "C-S-a") 'sp-beginning-of-sexp)
   (define-key sp-keymap (kbd "C-S-d") 'sp-end-of-sexp)
+
+  (define-key sp-keymap (kbd "C-M-e") 'sp-up-sexp)
+  (define-key sp-keymap (kbd "C-M-u") 'sp-backward-up-sexp)
+  (define-key sp-keymap (kbd "C-M-t") 'sp-transpose-sexp)
+
+  (define-key sp-keymap (kbd "C-M-n") 'sp-next-sexp)
+  (define-key sp-keymap (kbd "C-M-p") 'sp-previous-sexp)
+
+  (define-key sp-keymap (kbd "C-M-k") 'sp-kill-sexp) ;; FIXME: Doesn't work
+  (define-key sp-keymap (kbd "C-M-w") 'sp-copy-sexp)
   )
 
 (provide 'cnb-smartparens)
