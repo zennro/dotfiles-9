205c5dbfaa8ec0d3f6903eb10f0a85bbe8c63b80
Colin Bell <col@baibell.org>
2014-07-07 21:10:15 +1000

    Lein config

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,9 @@
-{:user { :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]]}
-        }
+{:user { :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]
+                   [lein-ancient "0.5.5"]
+                   [lein-annotations "0.1.0"]
+                   [lein-bikeshed "0.1.7"]]
+        :dependencies [[pjstadig/humane-test-output "0.6.0"]
+                       [slamhound "1.5.5"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
