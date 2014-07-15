1a39eefd73682eb57b284d4d0474f978cfd5fe60
Colin Bell <col@baibell.org>
2014-07-15 18:31:17 +1000

    EMACS - Remove unused code

--- emacs.d/cnb/cnb-sc.el
+++ emacs.d/cnb/cnb-sc.el
@@ -5,7 +5,7 @@
 ;; Taken out as it causes random "Selecting deleted buffer" errors
 
 (when (require 'git-gutter nil t)
-  ;(setq git-gutter-fr:side 'right-fringe);; Causes probs with flycheck if on LHS
+  ;;(setq git-gutter-fr:side 'right-fringe);; Causes probs with flycheck if on LHS
   (global-git-gutter-mode)
   ;;(git-gutter:linum-setup)
   )
