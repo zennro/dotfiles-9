ff9903c2c04ba5c273c2ad6557ee77702d56c4dc
Colin Bell <col@baibell.org>
2014-08-22 19:43:35 +1000

    LEIN

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user { :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]
+{:user { :plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
                    [lein-ancient "0.5.5"]
                    [lein-annotations "0.1.0"]
                    [lein-bikeshed "0.1.7"]
@@ -6,6 +6,8 @@
                    [lein-kibit "0.0.8"]
                    [codox "0.8.10"]
                    [lein-midje "3.1.3"]
-                   [jonase/eastwood "0.1.4"]]
+                   [jonase/eastwood "0.1.4"]
+                   [lein-try "0.3.2"]
+                   [lein-pprint "1.1.1"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
