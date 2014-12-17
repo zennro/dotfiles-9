3243c9dd962749a3f90dcbde1f03c02341ae3fb8
Colin Bell <col@baibell.org>
2014-12-17 20:44:16 +1100

    Emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,20 +1,13 @@
-;;(require 'cl)
-
-(setq dotfiles-dir
-      (file-name-directory
-       (or (buffer-file-name) load-file-name))) ;; Taken from emacs starter kit
-
 ;; I use keys that start with C-o for personal mappings.
 (global-unset-key "\C-o")
-
-(setq my-el-f (concat dotfiles-dir "colbell.el"))
-(setq my-org-f (concat dotfiles-dir "colbell.org"))
 
 ;; Use org-babel-load-file to compile colbell.org into colbell.el and
 ;; then load colbell.el. Only do this if colbell.el doesn't exist or
 ;; if colbell.org is newer than colbell.el.
-(if (or (not (file-exists-p my-el-f))
-        (time-less-p (nth 5 (file-attributes my-el-f))
-                     (nth 5 (file-attributes my-org-f))))
-    (org-babel-load-file my-org-f)
-  (load my-el-f))
+(let ((my-el-f (expand-file-name "colbell.el" user-emacs-directory))
+      (my-org-f (expand-file-name "colbell.org" user-emacs-directory)))
+  (if (or (not (file-exists-p my-el-f))
+          (time-less-p (nth 5 (file-attributes my-el-f))
+                       (nth 5 (file-attributes my-org-f))))
+      (org-babel-load-file my-org-f)
+    (load my-el-f)))
