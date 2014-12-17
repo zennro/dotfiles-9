223ca8878fa7d503f6a3a1ed03566d79c84ed34b
Colin Bell <col@baibell.org>
2014-12-17 19:14:17 +1100

    Convert config to an Org file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,16 +1,20 @@
-(require 'cl)
+;;(require 'cl)
 
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name))) ;; Taken from emacs starter kit
 (setq personal-dotfiles-dir (concat dotfiles-dir "cnb"))
 
-(autoload 'esup "esup" "Emacs Start Up Profiler." nil)
-
 ;; I use keys that start with C-o for personal mappings.
 (global-unset-key "\C-o")
 
-(setq load-prefer-newer t)
+(setq my-el-f (concat personal-dotfiles-dir "/init.el"))
+(setq my-org-f (concat personal-dotfiles-dir "/init.org"))
 
-(org-babel-load-file (concat personal-dotfiles-dir "/init.org"))
-
-(require 'cnb-lisp)
+;; Use org-babel-load-file to compile colbell.org into colbell.el and
+;; then load colbell.el. Only do this if colbell.el doesn't exist or
+;; if colbell.org is newer than colbell.el.
+(if (or (not (file-exists-p my-el-f))
+        (time-less-p (nth 5 (file-attributes my-el-f))
+                     (nth 5 (file-attributes my-org-f))))
+    (org-babel-load-file my-org-f)
+  (load my-el-f))
