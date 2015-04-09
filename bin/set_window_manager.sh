4c4e3ad819766561c2b5edb365d524d22774e2d1
Colin Bell <col@baibell.org>
2015-04-09 19:28:24 +1000

    Add bin/set_window_manager.sh

---
+++ bin/set_window_manager.sh
@@ -0,0 +1,8 @@
+# Link to this to tell KDE to use the
+# appropriate window manager.
+#
+# ln -s ~/.bin/set_window_manager.sh ~/.kde/env/set_window_manager.sh
+#
+export KDEWM=/usr/bin/xmonad
+#export KDEWM=/usr/bin/i3
+#export KDEWM=/usr/local/bin/spectrwm
