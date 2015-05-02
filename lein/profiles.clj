06f532168a22f7a4f8dbb460f24b6bbef69f15a3
Colin Bell <col@baibell.org>
2015-05-03 09:08:52 +1000

    Doc lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,18 +1,19 @@
 {:user
- { :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-             [lein-ancient "0.6.7"]
-             [lein-annotations "0.1.0"]
-             [lein-bikeshed "0.2.0"]
-             [lein-marginalia "0.8.0"]
-             [lein-kibit "0.1.2"]
-             [codox "0.8.11"]
-             [lein-midje "3.1.3"]
-             [jonase/eastwood "0.2.1"]
-             [lein-pprint "1.1.2"]
-             [com.aphyr/prism "0.1.3"]
-             [refactor-nrepl "1.0.4"]]
+ { :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"] ; nREPL middleware for cider
+             [lein-ancient "0.6.7"]               ; Looks for old libs
+             [lein-annotations "0.1.0"]           ; Displays comment annotations
+                                                  ; in proj
+             [lein-marginalia "0.8.0"]            ; Document CLJ
+             [lein-bikeshed "0.2.0"]              ; Code smells
+             [jonase/eastwood "0.2.1"]            ; CLJ lint tool
+             [lein-kibit "0.1.2"]                 ; Static code analyzer
+             [codox "0.8.11"]                     ; Generate API doc
+             [lein-midje "3.1.3"]                 ; Testing framework
+             [lein-pprint "1.1.2"]                ; Pretty pint
+             [com.aphyr/prism "0.1.3"]            ; Autorun tests
+             [refactor-nrepl "1.0.4"]]            ; Refactoring tools
 
-  :dependencies [[slamhound "1.5.5"]
-                 [com.aphyr/prism "0.1.3"]]
+  :dependencies [[slamhound "1.5.5"]              ; ns cleanup
+                 [com.aphyr/prism "0.1.3"]]       ; Autorun tests
 
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
